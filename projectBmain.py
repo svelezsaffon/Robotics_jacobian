a72de73be4f8865825dc863951dc85d48058fcaf
UTX-Shiro <spencer.carver@gmail.com>
2014-12-03 22:47:41 -0500

    added inverse jacobian options to main menu and updated header

--- projectBmain.py
+++ projectBmain.py
@@ -1,15 +1,18 @@
-#########################################################
-# Title       : CS569 OpenRave Project Part A           #
-#                                                       #
-# Author      : Spencer Carver                          #
-#                                                       #
-# Description : Basic Kinematic and Inverse Kinematic   #
-#               functionalities for the PUMA560 robotic #
-#               arm examined in class.                  #
-#########################################################
+##########################################################
+# Title       : CS569 OpenRave Project Part B            #
+#                                                        #
+# Author      : Spencer Carver &                         #
+#               Santiago Velez Saffon                    #
+#                                                        #
+# Description : Forward and Inverse Jacobian calculation #
+#               through 2 methods. Circular and Linear   #
+#               drawing applications to verify expected  #
+#               behavior of each method.                 #
+##########################################################
 
 import cross_product_mth
 import dtrm_approach
+import inverse_jacobian
 import circle_movement
 import line_movement
 
@@ -20,13 +23,15 @@ import time
 
 def main():
         menu = {}
-        menu['1']="Solve Jacobian (Vector-Cross Product Approach)" 
-        menu['2']="Solve Jacobian (DTRM Approach)"
-        menu['3']="Draw Circle (Inverse Kinematic)"
-        menu['4']="Draw Circle (Inverse Jacobian)" 
-        menu['5']="Draw Line (Inverse Kinematic)"
-        menu['6']="Draw Line (Inverse Jacobian)"
-        menu['7']="Exit"
+        menu['1']="Solve Jacobian (Vector-Cross Product)" 
+        menu['2']="Solve Jacobian (DTRM)"
+        menu['3']="Moore-Penrose Pseudo Inverse (Vector-Cross Product)"
+        menu['4']="Moore-Penrose Pseudo Inverse (DTRM)"
+        menu['5']="Draw Circle (Inverse Kinematic)"
+        menu['6']="Draw Circle (Inverse Jacobian)" 
+        menu['7']="Draw Line (Inverse Kinematic)"
+        menu['8']="Draw Line (Inverse Jacobian)"
+        menu['9']="Exit"
 
         env = Environment()
         env.SetViewer('qtcoin') # attach viewer (optional)
@@ -52,17 +57,23 @@ def main():
                         dtrm_approach.main()
                 elif selection == '3':
                         print ""
-                        circ.IK_move_in_circle()
+                        inverse_jacobian.main()
                 elif selection == '4':
                         print ""
-                        circ.Jac_move_in_circle()
+                        inverse_jacobian.main()
                 elif selection == '5':
                         print ""
+                        circ.IK_move_in_circle()
+                elif selection == '6':
+                        print ""
+                        circ.Jac_move_in_circle()
+                elif selection == '7':
+                        print ""
                         lin.IK_move_in_line()
-                elif selection == '6': 
+                elif selection == '8': 
                         print ""
                         lin.Jac_move_in_line()
-                elif selection == '7':
+                elif selection == '9':
                         env.Destroy()
                         break
                 else: 
